66f9a502ca703fa307aace25502999659d5499ac
Paul Magdon <pmagdon@gwdg.de>
2017-01-26 16:24:01 +0100

    local path added

--- webapp/gsg_shiny.r
+++ webapp/gsg_shiny.r
@@ -3,12 +3,32 @@ library(htmltools)
 library(htmlwidgets)
 library(shiny)
 
+######################
+
+source("gsg_grid/create_GSG.r")
+source("sampling/clustersampling.r")
+source("io/import.r")
+##########################
+
+
+df=data.frame()
+r=1
+
+country_code="DEU"
+adm_level="0"
+
+pdist.list=c(seq(from=400, to=500, by=100))# in m
+gdist.list=c(seq(from=50, to=60, by=10)) # in km
+germany=readRDS("/home/pmagdon/Downloads/DEU_adm0.rds") 
+boundary.geo = spTransform(germany, CRS("+init=epsg:4326"))
+GSG=generateGSG(distance=40,landpoly.geo=boundary.geo)
+
 # This tells htmlwidgets about our plugin name, version, and
 # where to find the script. (There's also a stylesheet argument
 # if the plugin comes with CSS files.)
 bingPlugin <- htmlDependency("leaflet.plugins", "2.0.0",
                             # src = c(href = "https://cdnjs.cloudflare.com/ajax/libs/leaflet-plugins/2.0.0/layer/tile/"),
-                             src = normalizePath('/media/data/tmp/'),
+                             src = normalizePath('webapp/'),
                              script = "Bing.min.js"
 )
 
@@ -45,8 +65,22 @@ server <- function(input, output, session) {
          {type: imagerySet});
      this.addLayer(bing);
  }")%>%
-      addMarkers(data = points())
+      addMarkers(data = GSG@points)
   })
 }
 
 shinyApp(ui, server)
+
+
+
+#######################
+#Method for importing wfs services
+#library("osmar")
+#library(gdalUtils)
+#library(rgdal)
+
+#dsn <- "WFS:http://efforts-webgis.uni-goettingen.de/geoserver/wfs?VERSION=1.0.0&SERVICE=WFS&REQUEST=GetCapabilities"
+#ogrinfo(dsn, so=TRUE)
+#ogr2ogr(dsn, "sic.shp", "admin_jambi_2011")
+#sic <- readOGR("sic.shp", "sic", stringsAsFactors=FALSE)
+#plot(sic)
